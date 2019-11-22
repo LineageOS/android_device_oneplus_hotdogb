d7616e10f9e239fa6fb3e44ced1e351858ed0f29
LuK1337 <priv.luk@gmail.com>
2019-11-22 01:14:07 +0100

    hotdog: Copy fstab to ramdisk

--- device.mk
+++ device.mk
@@ -24,6 +24,9 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.display.hotdog.rc
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
+
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_SHIPPING_API_LEVEL := 29
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
