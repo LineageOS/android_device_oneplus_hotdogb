48132325b9983b8c9e57c57b56e774ba51ce8220
LuK1337 <priv.luk@gmail.com>
2020-01-10 16:29:35 +0100

    hotdog: Import stock mixer paths

--- device.mk
+++ device.mk
@@ -27,6 +27,12 @@ AB_OTA_PARTITIONS += \
     recovery \
     vbmeta_system
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/mixer_paths_11811.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_11811.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_pahu.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_pahu.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_tavil.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil.xml
+
 # Camera
 PRODUCT_PACKAGES += \
     vendor.lineage.camera.motor@1.0 \
