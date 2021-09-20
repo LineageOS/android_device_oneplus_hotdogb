e3cc973c146a600dac610bed1d72121def812188
Bruno Martins <bgcngm@gmail.com>
2021-09-20 18:23:58 +0100

    hotdogb: Blob out ODM feature list
    There's not much of a purpose to have this in the device tree.
    
    Change-Id: I67751642b2f34576106a39a83e21996010215c3d

--- device.mk
+++ device.mk
@@ -26,10 +26,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
 
-# OPFeature
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/odm_feature_list:$(TARGET_COPY_OUT_ODM)/etc/odm_feature_list
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
